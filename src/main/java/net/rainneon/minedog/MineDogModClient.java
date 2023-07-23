1792e1b598a87904f57fb8d10bea21cfb49121e9
Yo x <715585973@qq.com>
2023-07-24 00:34:58 +0800

    ok

---
+++ src/main/java/net/rainneon/minedog/MineDogModClient.java
@@ -0,0 +1,10 @@
+package net.rainneon.minedog;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineDogModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/rainneon/minedog/MineDogModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rainneon.minedog;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MineDogModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
